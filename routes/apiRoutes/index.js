2cf51258e659d75467a31ee785216dfdf454a042
raedaltaki <raed.simon@gmail.com>
2021-02-19 12:17:18 -0500

    create API route

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const data = require('../../db/db.json');
+const saveToDB = require('../../lib/notes');
+
+router.get('/notes', (req,res)=>
+{
+    res.json(data);
+});
+
+router.post('/notes', (req,res)=>
+{
+    req.body.id = data.length.toString();
+    
+    data.push(req.body);
+    saveToDB(data);
+    res.json(data);
+});
+
+router.delete('/notes/:id', (req,res)=>
+{
+    const index = data.findIndex(note => note.id == req.params.id );
+    if(index === -1)
+    {
+        res.send('ID is not available')
+    }
+    else
+    {
+        data.splice(index,1);
+        saveToDB(data);
+        res.json(data);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// parse incoming JSON data
+app.use(express.json());
+
 app.use('/',htmlRoutes);
+app.use('/api/', apiRoutes);
 
 
 app.listen(PORT , () => 
